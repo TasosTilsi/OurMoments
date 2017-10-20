b78c8be2c22b56d66824a4d48f31fc07c3c386dd
tasos <tasostilsi@hotmail.com>
2017-10-20 22:16:30 +0300

    Second commit Made Home Screen and the transition in the ImagesView

--- app/src/main/java/com/katerina/tasos/ourmoments/MainActivity.java
+++ app/src/main/java/com/katerina/tasos/ourmoments/MainActivity.java
@@ -1,73 +1,20 @@
 package com.katerina.tasos.ourmoments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import java.util.ArrayList;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    private final String image_titles[] = {
-            "Img1",
-            "Img2",
-            "Img3",
-            "Img4",
-            "Img5",
-            /*"Img6",
-            "Img7",
-            "Img8",
-            "Img9",
-            "Img10",
-            "Img11",
-            "Img12",
-            "Img13",*/
-    };
-
-    private final Integer image_ids[] = {
-            R.drawable.img1,
-            R.drawable.img2,
-            R.drawable.img3,
-            R.drawable.img4,
-            R.drawable.img5,
-            /*R.drawable.img6,
-            R.drawable.img7,
-            R.drawable.img8,
-            R.drawable.img9,
-            R.drawable.img10,
-            R.drawable.img11,
-            R.drawable.img12,
-            R.drawable.img13,*/
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.imageGallery);
-        recyclerView.setHasFixedSize(true);
-
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 3);
-        recyclerView.setLayoutManager(layoutManager);
-
-        ArrayList<CreateList> createLists = prepareData();
-        CustomAdapter adapter = new CustomAdapter(getApplicationContext(), createLists);
-        recyclerView.setAdapter(adapter);
-
-
     }
 
-    private ArrayList<CreateList> prepareData() {
-
-        ArrayList<CreateList> theImage = new ArrayList<>();
-        for (int i = 0; i < image_titles.length; i++) {
-            CreateList createList = new CreateList();
-            createList.setImage_title(image_titles[i]);
-            createList.setImage_ID(image_ids[i]);
-            theImage.add(createList);
-        }
-        return theImage;
+    public void ChangeActivity(View view) {
+        Intent myIntent = new Intent(MainActivity.this, ImagesActivity.class);
+        MainActivity.this.startActivity(myIntent);
     }
 }
